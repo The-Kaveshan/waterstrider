8262cc61625b4905419131e0685c8e66f64b6fb4
The-Kaveshan <kaveshank@gmail.com>
2024-06-23 10:40:26 +0200

    implemented nextjs and tailwindui

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Hero } from "@/components/Hero";
+import { Header } from "@/components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+}
