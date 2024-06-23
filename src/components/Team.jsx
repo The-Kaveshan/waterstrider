66fcae254001ed05f739a2e05cf6ec6a2c88cc9e
Kavi <kavi.kistan@onespark.co.za>
2024-06-23 18:05:40 +0200

    Team slider

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,11 +1,12 @@
-import { useId } from "react";
-
+"use client";
+import { useEffect, useId, useRef, useState } from "react";
 import { Container } from "@/components/Container";
 import Image from "next/image";
-
 import helen from "@/images/helen.jpg";
 import sashnee from "@/images/sashnee.jpg";
 import rochelle from "@/images/rochelle.jpg";
+
+import Swiper, { Navigation, Pagination } from "swiper";
 
 const people = [
   {
@@ -28,56 +29,104 @@ const people = [
   },
 ];
 
-export function TheTeam() {
+function TeamMobile() {
+  useEffect(() => {
+    Swiper.use([Navigation, Pagination]);
+
+    const swiper = new Swiper(".swiper-container", {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 0,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+    return () => {
+      if (swiper && swiper.destroy) {
+        swiper.destroy(true, true);
+      }
+    };
+  }, []);
+
+  return (
+    <div className="swiper-container">
+      <div className="swiper-wrapper">
+        {people.map((person) => (
+          <div
+            key={person.name}
+            className="swiper-slide items-center justify-center h-auto w-100  px-4"
+          >
+            <Image
+              src={person.image}
+              className="aspect-[14/13] w-full rounded-2xl object-cover "
+              alt={person.image}
+              height={100}
+              width={100}
+            />
+            <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight">
+              {person.name}
+            </h3>
+            <p className="text-base leading-7 text-gray-600">{person.role}</p>
+            <p className="text-sm leading-6 text-gray-400">{person.bio}</p>
+          </div>
+        ))}
+      </div>
+      <div class="swiper-pagination"></div>
+    </div>
+  );
+}
+
+export function Team() {
   return (
     <section
       id="team"
       aria-label="Features for building a portfolio"
       className="py-20 sm:py-32"
     >
-      <Container>
-        <div className="mx-auto max-w-2xl sm:text-center">
+      <Container className="px-0">
+        <div className="mx-auto max-w-2xl sm:text-center px-4">
           <h2 className="text-3xl font-medium tracking-tight text-gray-900">
             Meet Our Team
           </h2>
-          <p className="mt-2 text-lg text-gray-600">
+          <p className="mt-2 text-lg text-gray-400">
             We're a dynamic group of individuals who are passionate about what
             we do and dedicated to delivering the best results for our clients.
             Each member brings unique skills and insights, ensuring that we
             provide top-notch service and innovative solutions.
           </p>
         </div>
-        <ul
-          role="list"
-          className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 text-sm sm:mt-20 sm:grid-cols-2 md:gap-y-10 lg:max-w-none lg:grid-cols-3"
-        >
-          {people.map((person) => (
-            <li key={person.name}>
-              <Image
-                src={person.image}
-                className="aspect-[14/13] w-full rounded-2xl object-cover"
-                alt={person.image}
-              />
-              <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight">
-                {person.name}
-              </h3>
-              <p className="text-base leading-7 text-gray-300">{person.role}</p>
-              <p className="text-sm leading-6 text-gray-500">{person.bio}</p>
-            </li>
-          ))}
-          {/* {features.map((feature) => (
-            <li
-              key={feature.name}
-              className="rounded-2xl border border-gray-200 p-8"
-            >
-              <feature.icon className="h-8 w-8" />
-              <h3 className="mt-6 font-semibold text-gray-900">
-                {feature.name}
-              </h3>
-              <p className="mt-2 text-gray-700">{feature.description}</p>
-            </li>
-          ))} */}
-        </ul>
+        <div className="mt-16 md:hidden">
+          <TeamMobile />
+        </div>
+        <Container className="hidden md:mt-20 md:block">
+          <ul
+            role="list"
+            className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 text-sm sm:mt-20 sm:grid-cols-2 md:gap-y-10 lg:max-w-none lg:grid-cols-3"
+          >
+            {people.map((person) => (
+              <li key={person.name}>
+                <Image
+                  src={person.image}
+                  className="aspect-[14/13] w-full rounded-2xl object-cover"
+                  alt={person.image}
+                />
+                <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight">
+                  {person.name}
+                </h3>
+                <p className="text-base leading-7 text-gray-600">
+                  {person.role}
+                </p>
+                <p className="text-sm leading-6 text-gray-400">{person.bio}</p>
+              </li>
+            ))}
+          </ul>
+        </Container>
       </Container>
     </section>
   );
