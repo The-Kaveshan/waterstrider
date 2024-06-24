ca86a4015ec488057e60bc6c55b173a36727fda4
Kavi <kavi.kistan@onespark.co.za>
2024-06-24 10:13:12 +0200

    Footer clean up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,6 @@ export function Footer() {
     <section id="footer">
       <Container className="px-0  max-w-none relative">
         <div className="relative z-10 flex flex-col items-center pb-12 pt-8 md:flex-row-reverse md:justify-between md:pt-6">
-          <p className="mt-6 text-sm text-gray-500 md:mt-0">
-            &copy; Copyright {new Date().getFullYear()}. All rights reserved.
-          </p>
           <div className="flex items-center text-gray-900">
             <Logomark className="h-10 w-10 flex-none fill-cyan-500" />
             <div className="ml-4">
@@ -17,6 +14,9 @@ export function Footer() {
               <p className="mt-1 text-sm">Data-Driven Solutions</p>
             </div>
           </div>
+          <p className="mt-6 text-sm sm:text-gray-500 xs:text-black md:mt-0">
+            &copy; Copyright {new Date().getFullYear()}. All rights reserved.
+          </p>
         </div>
         <div className="absolute bottom-0 left-0 w-full h-32 z-0">
           <Image src={waves} alt={"waves"} layout="fill" objectFit="cover" />
